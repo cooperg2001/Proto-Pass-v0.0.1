5268fb0b83afb17489a3e8b555adaac96f3298bf
cooperg2001 <KoebelStudios@gmail.com>
2016-07-07 20:37:06 -0400

    added encryption
    added basic encryption using XOR

--- main.cpp
+++ main.cpp
@@ -2,16 +2,47 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
+
 using namespace std;
 
-int main()
 
+string c;
+
+
+int encrypt()
+{
+   string toEncrypt = c;
+ char keyToEncrypt = 's'; //115 in ascii
+
+ for (int temp = 0; temp < toEncrypt.size(); temp++)
+   toEncrypt[temp] ^= keyToEncrypt;
+ cout << "nThe encrypted data = " << toEncrypt << endl;
+c = toEncrypt;
+cout << c << endl;
+
+return 0;
+
+}
+
+int decrypt()
+{
+    string toEncrypt = c;
+ char keyToEncrypt = 's'; //115 in ascii
+
+  for (int temp = 0; temp < toEncrypt.size(); temp++)
+   toEncrypt[temp] ^= keyToEncrypt;
+ cout << "nThe unencrypted data = " << toEncrypt << endl;
+ c = toEncrypt;
+
+ return 0;
+}
+int main()
 
 {
 //Declares variables
-   string a;
-   string b;
-   string c;
+string a;
+string b;
+
 //Reads from stored password and sets c to contents
 string line_;
 ifstream file_("pass.txt");
@@ -19,17 +50,20 @@ if(file_.is_open())
 {
     while(getline(file_,line_))
     {//line below is used for testing
-       // cout << line_ << '\n';
+        //cout << line_ << '\n';
     }
     file_.close();
-    c = atoi(line_.c_str());
+    c = line_;
+    decrypt();
 }
 
 else
    {
       cout << "file is not open" << '\n';
    }
+
 //Standard double input confirmation
+//if the first entry match's c program terminates
     cout << "Enter a New OR Existing Password" << endl;
     cin >> a;
     if (a==c)
@@ -45,12 +79,14 @@ else
     if (a==b)
     {
     c = a;
-    cout << "Number " << c << " Stored" << endl;
+    encrypt();
+    cout << "Password " << c << " Storing" << endl;
  ofstream myfile;
  myfile.open ("pass.txt");
  myfile << c;
- myfile.close();
 
+ myfile.close();
+cout << "Password " << c << " Stored" << endl;
     }
      //If a does not match b terminates program
     else
